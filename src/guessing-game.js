ca826dd7a205aa532f77b14242200c7103f86a12
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2021-09-02 12:34:27 +0300

    fix: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,29 @@
 class GuessingGame {
+   
     constructor() {}
 
     setRange(min, max) {
+        
+this.min = min;
+this.max = max;
 
     }
 
     guess() {
-
+        this.result = Math.round((this.max + this.min) / 2)
+        return this.result;
+        
     }
-
+   
     lower() {
+        this.max = this.result;
+
 
     }
 
     greater() {
+   this.min = this.result;
+
 
     }
 }
